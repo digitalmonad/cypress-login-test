c2f253188d2cd3467406af12491ceefa51d28b92
Pavel Kocman <kocmanpavel@gmail.com>
2020-12-08 14:56:30 +0100

    test if workflow is passing

---
+++ cypress/e2e/actions.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+context("Test the test", () => {
+  // https://on.cypress.io/interacting-with-elements
+
+  it("is working", () => {
+    expect(true).to.be.true;
+  });
+});
